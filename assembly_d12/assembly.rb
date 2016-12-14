ff93e9da3316820d86c832e5872f4392c93b2bf4
Timothy Nordloh <tnordloh@gmail.com>
2016-12-13 22:12:02 -0700

    cleaned up

--- assembly_d12/assembly.rb
+++ assembly_d12/assembly.rb
@@ -1,15 +1,22 @@
 
 class Assembly
-  def initialize(instructions,registers)
+  def initialize(instructions,cpu = nil)
     @instructions = instructions
+    @instructions << "EOF"
     @position = 0
 
-    @registers = registers || {
+    @cpu = cpu || {
       "a" => 0,
       "b" => 0,
       "c" => 0,
       "d" => 0
     }
+  end
+  attr_reader :cpu, :instructions
+  attr_accessor :position
+
+  def register?(value)
+    cpu.keys.include?(value)
   end
 
   def parse(instruction)
@@ -17,55 +24,52 @@ class Assembly
   end
 
   def dec(register)
-    @registers[register] -= 1
-    @position + 1
+    cpu[register] -= 1
+    position + 1
   end
 
   def inc(register)
-    @registers[register] += 1
-    @position + 1
+    cpu[register] += 1
+    position + 1
   end
 
   def cpy(source,destination)
-    if source.to_i > 0
-      @registers[destination] = source.to_i
+    if register?(source)
+      cpu[destination] = cpu[source]
     else
-      @registers[destination] = @registers[source]
+      cpu[destination] = source.to_i
     end
-    @position + 1
+    position + 1
   end
 
   def jnz(register, distance)
-    if ("a".."d").include?(register) && @registers[register] != 0 
-      @position + distance.to_i 
+    if register?(register) && cpu[register] != 0 
+      position + distance.to_i 
     elsif register.to_i != 0
-      @position + distance.to_i
+      position + distance.to_i
     else
-      @position + 1
+      position + 1
     end
   end
 
   def execute
-    while @position < @instructions.size
-#      p @position
-#      p @registers
-      i = parse(@instructions[@position])
-#      p i
+    while instructions[position] != "EOF"
+      i = parse(instructions[position])
       @position = self.send(*i)
     end
-    @registers
+    cpu
   end
 
 end
 
 if $0 == __FILE__
   instructions = ARGF.readlines 
-    registers = { 
+    cpu = { 
       "a" => 0,
       "b" => 0,
       "c" => 1,
       "d" => 0
     }
-  a = Assembly.new(instructions,registers)
+  a = Assembly.new(instructions,cpu)
   p a.execute
 end
--- assembly_d12/assembly_test.rb
+++ assembly_d12/assembly_test.rb
@@ -4,10 +4,11 @@ require 'minitest/autorun'
 require_relative 'assembly'
 describe Assembly do
   it "takes an input, produces correct output" do
-    instructions = "cpy 41 a\ninc a\ninc a\ndec a\njnz a 2\ndec a".split("\n")
+    instructions = "cpy 41 a\ninc a\ninc a\ndec a\njnz a 2\ndec a\nEOF".split("\n")
 
     a = Assembly.new(instructions)
-    a.execute.must_equal([42,0,0,0])
+    return_value = Hash[['a',"b",'c','d'].zip([42,0,0,0])] 
+    a.execute.must_equal(return_value)
     
   end
 end
