01c9ab371e99fe7d91d6fc26b6454a9ea2a4fd32
Timothy Nordloh <tnordloh@gmail.com>
2016-12-16 18:38:54 -0700

    five minute bonus

--- slots_d15/slots.rb
+++ slots_d15/slots.rb
@@ -1,13 +1,13 @@
 class Slots
   def initialize
     @wheels = []
+    @normalized = false
   end  
   attr_reader :wheels, :biggest_wheel
   attr_writer :wheels
 
   def max
-    @biggest_wheel ||= 
-      @wheels.max_by { |wheel| wheel.size }
+    @biggest_wheel ||= @wheels.max_by { |wheel| wheel.size }
       .size
   end
 
@@ -16,6 +16,9 @@ class Slots
   end
 
   def normalize
+    return wheels if @normalized == true
+    @normalized = true
+    p "normalizing"
     biggest = max
     wheels.map! { |wheel| 
       div,mod = biggest.divmod(wheel.size)
@@ -25,43 +28,41 @@ class Slots
   end
 
   def rotate(num)
-    wheels.map { |wheel|
-      wheel.rotate(num)
-    }
+    wheels.map { |wheel| wheel.rotate(num) }
   end
 
   def combination?(num)
-    rotate(num)
+    normalize
+      rotate(num)
       .each_with_index
       .map { |wheel,i| wheel[(i % wheel.size) + 1] == 0 }
       .all?
+  end
+
+  def find_solution
+    (0..Float::INFINITY).find_index { |i| combination?(i) }
   end
 end
 
 
 if $0 == __FILE__
+  test_data = [ [5,4], 
+                [2,1] ]
+  submission_data = [ [17,1], 
+                      [7, 0],
+                      [19,2],
+                      [5, 0],
+                      [3, 0],
+                      [13,5],
+  ]
   w = Slots.new
-  w.add(17,1)
-  w.add(7,0)
-  w.add(19,2)
-  w.add(5,0)
-  w.add(3,0)
-  w.add(13,5)
-  w.add(11,0)
-#  w.add(5,4)
-#  w.add(2,1)
-  p w.rotate(1)
-  p w.normalize
-  p w.combination?(4)
-  p w.combination?(5)
-  p w.rotate(5)
-  (0..Float::INFINITY).each do |i|
-    p i
-    if w.combination?(i)
-      puts "#{w.rotate(i).map { |w| w.join(",") }.join("\n")}"
-      puts "solution is #{i}"
-      break
-    end
-  end
-
+  test_data.each {|t| w.add(*t) }
+  puts "test solution is #{w.find_solution}"
+  w = Slots.new
+  submission_data.each {|t| w.add(*t) }
+  puts "first solution is #{w.find_solution}"
+  w = Slots.new
+  submission_data.each {|t| w.add(*t) }
+  w.add(*[11,0])
+  puts "second solution is #{w.find_solution}"
 end
