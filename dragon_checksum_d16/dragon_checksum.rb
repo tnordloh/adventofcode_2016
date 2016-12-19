432413a7414afed1eff7248fe126a9a98141a85d
Timothy Nordloh <tnordloh@gmail.com>
2016-12-18 20:48:25 -0700

    intention revealing names

--- dragon_checksum_d16/dragon_checksum.rb
+++ dragon_checksum_d16/dragon_checksum.rb
@@ -1,22 +1,31 @@
-module DragonChecksum
+module EraseMyTracks
   module_function
-  def next(seed)
-    "#{seed}0#{seed.chars.reverse.map {|c| c == "0" ? "1" : "0" }.join}"
-  end
-  
-  def checksum(data)
-    begin
-      data = data.chars.each_slice(2).map { |x,y| x == y ? "1" : "0" }.join
-    end while data.size % 2 == 0
-    data
-  end
+	def dragon_curve(seed,minimum_length)
+		while seed.size < minimum_length 
+			seed = "#{seed}0#{next_curve(seed)}"
+			return seed[0,minimum_length] if seed.size >= minimum_length
+		end
+	end
+
+	def next_curve(current_curve)
+		current_curve
+			.chars
+			.reverse
+			.map {|c| c == "0" ? "1" : "0" }
+			.join
+	end
+
+	def checksum(data)
+		loop do
+			data = data.chars.each_slice(2).map { |x,y| x == y ? "1" : "0" }.join
+			return data if data.size.odd?
+		end
+	end
 end
 
 if $0 == __FILE__
-  p output = "01110110101001000"
-  p length = 35_651_584
-  while output.size < length 
-    output = DragonChecksum.next(output)
-  end
-  p DragonChecksum.checksum(output[0,length])
+	p output = "01110110101001000"
+	p length = 35_651_584
+  output = EraseMyTracks.dragon_curve(output,length)
+	p EraseMyTracks.checksum(output)
 end
--- dragon_checksum_d16/runme.rb
+++ dragon_checksum_d16/runme.rb
@@ -2,10 +2,7 @@
 
 require_relative 'dragon_checksum'
 
-
-p output = ARGV[0]
-p length = ARGV[1].to_i
-while output.size < length 
-	output = DragonChecksum.next(output)
-end
-p DragonChecksum.checksum(output[0,length])
+output = ARGV[0]
+length = ARGV[1].to_i
+output = EraseMyTracks.dragon_curve(output,length)
+puts EraseMyTracks.checksum(output)
