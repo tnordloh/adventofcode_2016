7473980970a69cabf050396285624883b88e4d92
Timothy Nordloh <tnordloh@gmail.com>
2016-12-21 12:19:43 -0700

    solved, 2 minutes remaining

--- hopscotch_d17/trapped_floor_d18/trapped_floor.rb
+++ hopscotch_d17/trapped_floor_d18/trapped_floor.rb
@@ -2,10 +2,22 @@ module TrappedFloor
   module_function
   
   def next(row)
-    calculate_tile("." + row[0,2]) + 
+    calculate_first_tile(    row) +
+      calculate_middle_tiles(row) +
+      calculate_last_tile(   row)
+  end
+
+  def calculate_middle_tiles(row)
     row.scan( /(.)(?=(..))/ ).map do |tiles|
       calculate_tile(tiles.join)
-    end.join + 
+    end.join 
+  end
+
+  def calculate_first_tile(row)
+    calculate_tile("." + row[0,2])
+  end
+
+  def calculate_last_tile(row)
     calculate_tile(row[-2,2] + "." )
   end
 
