9dbfe907730a09808fd09ababed44719eed95242
Timothy Nordloh <tnordloh@gmail.com>
2016-12-22 22:30:46 -0700

    fixed inject 9:00 remaining

--- blocked_ips_d20/blocked_ips.rb
+++ blocked_ips_d20/blocked_ips.rb
@@ -5,16 +5,19 @@ module BlockedIps
     ranges.sort { |one,two| one.min <=> two.min }
   end
 
+  def overlap_upper_bound(ranges)
+    ranges.max_by {|x| x.max }.max
+  end
+
   def consolidate_overlaps(ranges)
     range_changed = 0
     while range_changed != ranges.size
       range_changed = ranges.size
       ranges = sort(ranges)
-      .chunk_while { |first,second| first.max + 1 >= second.min }
-      .map  { |overlapping_ranges| 
-        highest = overlapping_ranges.max_by {|x| x.max }.max
-        (overlapping_ranges.first.min..highest)
-      }
+        .chunk_while { |first,second| first.max + 1 >= second.min }
+        .map  { |overlapping_ranges| 
+          (overlapping_ranges.first.min..overlap_upper_bound(overlapping_ranges))
+        }
     end
     ranges
   end
